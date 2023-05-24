56b85c2735f0bd4544b2872d2127439477e3eb5a
Pedro Zambudio <pjzambudio@gmail.com>
2023-05-24 12:50:12 +0200

    06.BACKEND - Refactorización - Controlador genérico

--- Application/Dtos/CategoryDto.cs
+++ Application/Dtos/CategoryDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace bootcamp_store_backend.Application.Dtos
 {
-    public class CategoryDto
+    public class CategoryDto: IDto
     {
         public long Id { get; set; }
         public string Name { get; set; }
---
+++ Application/Dtos/IDto.cs
@@ -0,0 +1,7 @@
+﻿namespace bootcamp_store_backend.Application.Dtos
+{
+    public interface IDto
+    {
+        long Id { get; set; }
+    }
+}
